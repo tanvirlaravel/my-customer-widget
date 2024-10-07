1fc6bd3907904fe3615f91b1a19ca3338854ade3
Tanvir <tanvirlaravel@gmail.com>
2024-10-07 12:25:25 +0600

    construtor method added

--- My_Custom_Widget.php
+++ My_Custom_Widget.php
@@ -1,5 +1,14 @@
 <?php 
 
 class My_Custom_Widget extends WP_Widget {
-    
+    public function __construct()
+    {
+        parent::__construct(
+            "my_custom_widget",
+            "My Custom Widget",
+            array(
+                "description" => "Display recent post and static message.",
+            ),
+        );
+    }
 }
