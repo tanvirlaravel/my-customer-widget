22e26010a83dbc70109b055447c169909a4512b6
Tanvir <tanvirlaravel@gmail.com>
2024-10-07 11:54:02 +0600

    initail commit

---
+++ my-customer-widget.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * Plugin Name: My Custom Widget
+ * Description: This is my Custom Widget
+ * Author: Md Tanvirul Islam
+ * Version: 1.0.0
+ * Author URI: www.tanvir.com
+ * Plugin URI: https/example.com/custom-widget
+ */
+
+ /** *****************************************************************
+  *                        Basic Shortcode
+ *********************************************************************/ 
